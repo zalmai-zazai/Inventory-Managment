3da9828dcb4b6a0b8efd27caa8087e5dce54798d
zalmai-zazai <shahzalmai98@gmail.com>
2025-07-16 15:43:21 -0700

    final deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SellerPage from "./Pages/SellerPage";
 
--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../Context/AuthContext";
 
 export default function PrivateRoute({ children }) {
   const { currentUser } = useAuth();
