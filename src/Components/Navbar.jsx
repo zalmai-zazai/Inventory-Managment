4ac653962fefacc3696be9a0adbf275a68211af6
zalmai-zazai <shahzalmai98@gmail.com>
2025-07-16 15:40:47 -0700

    navbar solved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../Context/AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
